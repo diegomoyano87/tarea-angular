aacce9ca87e52f2c9b6cf84b61da163e460b5980
diegomoyano87 <diegomoyano_19@hotmail.com>
2023-07-14 00:54:44 -0300

    Realizacion del arreglo con el boton para que aparezca y desaparezca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+type Producto = {
+  nombre: string;
+  costo: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tarea-angular';
-}
+  verProducto = true;  
+  productos: Producto[] = [
+    {
+      nombre: 'Lavarropa',
+      costo:'45000',
+    },
+    {
+      nombre: 'Heladera',
+      costo:'60000',
+    },
+    {
+      nombre: 'Cocina',
+      costo:'50000',
+    },
+    {
+      nombre: 'Aspiradora',
+      costo:'15000',
+    },
+       ] 
+    
+      }
+
+      
+
+   
+  
